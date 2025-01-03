d4cc031af54328affb8c6d4fab41bd0fe41cdc2f
Enukio <38923168+Enukio@users.noreply.github.com>
2025-01-03 22:19:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ from utils.core import process
 
 
 async def main():
-    try:
-        await process()
-    except KeyboardInterrupt:
-        print("Program interrupted. Exiting gracefully...")
-    finally:
-        print("Cleaning up resources before exiting.")
-
+    await process()
 
 if __name__ == '__main__':
     try:
